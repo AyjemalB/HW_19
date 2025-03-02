f88010cf625c490320dd7d7537b7b3b23f7d6862
AyjemalB <ayjemal908@outlook.com>
2025-03-02 19:42:34 +0100

    Change way adresse

--- first_directory/script_create_random_file.sh
+++ first_directory/script_create_random_file.sh
@@ -4,6 +4,6 @@
 # Create 100 file with random name 
 for i in {1..100};
 do
-	echo "File content $RANDOM" >/first_directory/$RANDOM
+	echo "File content $RANDOM" >/root/HW_19/first_directory/$RANDOM
 done
 
--- second_directory/script_mv_even_files.sh
+++ second_directory/script_mv_even_files.sh
@@ -2,10 +2,10 @@
 
 # mv even files other directory
 
-for file in /first_directory/*;
+for file in /root/first_directory/*;
 do
 	filename=$(basename "$file")
 	if ((filename % 2 == 0));then
-		mv "$file" /second_directory/
+		mv "$file" /root/second_directory/
 	fi
 done
